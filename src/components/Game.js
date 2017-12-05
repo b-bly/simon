7df30553bf4c3c00f0bca70ae599c656b6c1049e
Brendt Bly <brendt.bly@outlook.com>
2017-12-05 17:00:38 -0600

    board added

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,35 +1,57 @@
 import React, { Component } from 'react';
 
+import { Square } from './Square';
+
+// bootstrap
+import { Button } from 'react-bootstrap';
+import { Grid } from 'react-bootstrap';
+import { Row } from 'react-bootstrap';
+import { Col } from 'react-bootstrap';
+// const Grid = ReactBootstrap.Grid;
+// const Row = ReactBootstrap.Row;
+// const Col = ReactBootstrap.Col;
+
 // This was helpful: 
 // https://blog.lavrton.com/using-react-with-html5-canvas-871d07d8d753
 export class Game extends Component {
     constructor(props) {
         super(props);
         this.style = {
-            backgroundColor: 'gray'
-        }
-    }
-    
-        componentDidMount() {
-            this.updateCanvas();
-        }
-        componentDidUpdate() {
-            this.updateCanvas();
-        }
-        updateCanvas() {
-            const ctx = this.refs.canvas.getContext('2d');
-            ctx.clearRect(0,0, 300, 300);
-            // draw children “components”
-            //rect({ctx, x: 10, y: 10, width: 50, height: 50});
-        }
-        render() {
-             return (
-                 <canvas ref="canvas" width={300} height={300} style={this.style}/>
-             );
+            position: 'relative',
+            backgroundColor: 'gray',
+            padding: 10,
+            width: 500,
+            height: 500
+        };
+        this.center = {
+            display: 'flex',
+            justifyContent: 'center'
         }
     }
 
-    function rect(props) {
-        const {ctx, x, y, width, height} = props;
-        ctx.fillRect(x, y, width, height);
+    componentDidMount() {
+        this.updateCanvas();
     }
+    componentDidUpdate() {
+        this.updateCanvas();
+    }
+    updateCanvas() {
+
+    }
+    render() {
+        return (
+            <div>
+                <div className={'container'} style={ this.center }>
+                    <div className={'col text-center'} style={this.style}>
+                        <Square />
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+function rect(props) {
+    const { ctx, x, y, width, height } = props;
+    ctx.fillRect(x, y, width, height);
+}
---
+++ src/components/Square.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+
+export class Square extends Component {
+    constructor(props) {
+        super(props);
+        this.style = {
+            width: 50,
+            height: 50,
+            backgroundColor: 'blue',
+            position: 'fixed'
+        }
+    }
+
+    componentDidMount() {
+     
+    }
+    componentDidUpdate() {
+
+    }
+    render () {
+        return (
+            <div style={this.style}>
+                </div>
+        );
+    }
+}
+
+function rect(props) {
+    const {ctx, x, y, width, height} = props;
+    ctx.fillRect(x, y, width, height);
+}
